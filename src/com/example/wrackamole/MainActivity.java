2c9f8aea62a9088a5dd68c13c7f934f2d7539cd7
Witt <ferseroz@hotmail.com>
2014-07-09 05:30:59 -0400

    Commit e sus

--- src/com/example/wrackamole/MainActivity.java
+++ src/com/example/wrackamole/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.wrackamole;
 
 import android.app.Activity;
-<<<<<<< HEAD
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.view.LayoutInflater;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-=======
->>>>>>> FETCH_HEAD
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
